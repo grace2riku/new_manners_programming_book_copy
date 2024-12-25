13283d1d369da7cf4ac3d00e7a39014ffe8b38de
Koji Abe <gracehime@gmail.com>
2024-12-25 12:55:36 +0900

    Listing 2.3 配列変数の使用例の追加

---
+++ chap2/code2-3.c
@@ -0,0 +1,13 @@
+// Listing 2.3 配列変数の使用例
+
+#include <stdio.h>
+
+int main(void) {
+    int no;
+    int score[5] = {80, 65, 70, 93, 77};
+    printf("Student number? (0-4): ");
+    scanf("%d", &no);
+    printf("Score: %d\n", score[no]);
+
+    return 0;
+}
