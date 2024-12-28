f20dcd253ac797849bd6c2678debcec16d9ad17b
Koji Abe <gracehime@gmail.com>
2024-12-28 16:27:02 +0900

    Listing 3.1 加算を行うCプログラムの追加

---
+++ chap3/c3-1.c
@@ -0,0 +1,8 @@
+// Listing 3.1 加算を行うCプログラム
+#include <stdio.h>
+
+int main(void) {
+    int a = 2;
+    printf("%d\n", a+3);
+    return 0;
+}
---
+++ chap3/c3-1.s
@@ -0,0 +1,31 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 13, 0	sdk_version 14, 2
+	.globl	_main                           ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	movl	$2, -8(%rbp)
+	movl	-8(%rbp), %esi
+	addl	$3, %esi
+	leaq	L_.str(%rip), %rdi
+	movb	$0, %al
+	callq	_printf
+	xorl	%eax, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"%d\n"
+
+.subsections_via_symbols
