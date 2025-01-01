41e63e198ddb7a52086c61115796dfe016854cf0
Koji Abe <gracehime@gmail.com>
2025-01-01 16:10:52 +0900

    Listing 3.16 動的に構造体のデータ確保するCプログラムの追加

---
+++ chap3/c3-16.c
@@ -0,0 +1,19 @@
+// Listing 3.16 動的に構造体のデータ確保するCプログラム
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dict {
+    int data;
+    struct dict *next;
+} Dict;
+
+
+int main(void) {
+    Dict *first = malloc(sizeof(Dict));
+    first->data = 5;
+    Dict *second = malloc(sizeof(Dict));
+    first->next = second;
+    second->data = 8;
+    second->next = NULL;
+    return 0;
+}
