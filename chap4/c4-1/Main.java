fe376ea1dc2970a0fcf9f35a0bd802d9a61c733f
Koji Abe <gracehime@gmail.com>
2025-01-05 10:46:28 +0900

    Listing 4.1 入力・演算・出力を行うJavaプログラムの追加

---
+++ chap4/c4-1/Main.java
@@ -0,0 +1,16 @@
+// Listing 4.1 入力・演算・出力を行うJavaプログラム
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        int amount;
+        final var price = 150;
+  
+        System.out.print("How many do you need?: ");
+        amount = sc.nextInt();
+        System.out.println("Total : %d yen".formatted(price * amount));
+  
+        sc.close();
+    }
+}
