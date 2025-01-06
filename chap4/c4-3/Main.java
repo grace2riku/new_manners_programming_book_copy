770bc814c3633fa0ffe34455f6c1a96fe424695e
Koji Abe <gracehime@gmail.com>
2025-01-06 23:29:02 +0900

    Listing 4.3 Studentクラスを利用するコードの追加

---
+++ chap4/c4-3/Main.java
@@ -0,0 +1,7 @@
+// Listing 4.3 Studentクラスを利用するコード
+public class Main {
+    public static void main(String[] args) {
+        var s1 = new Student("Masa", 70);
+        s1.printData();        
+    } 
+}
