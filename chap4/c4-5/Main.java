a6dbbaba64513ae52fbb72ac15ce6286c781abbf
Koji Abe <gracehime@gmail.com>
2025-01-07 10:12:01 +0900

    Listing 4.5 例外処理を行うプログラム例の追加

---
+++ chap4/c4-5/Main.java
@@ -0,0 +1,25 @@
+// Listing 4.5 例外処理を行うプログラム例
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        var x = 0;
+        do {
+            System.out.print("Enter a positive integer: ");
+            try {
+                x = sc.nextInt();
+                if (x <= 0) {
+                    System.out.println("Input positive integer!");
+                }    
+            } catch (InputMismatchException e) {
+                System.out.println(e);
+                sc.next();
+            }
+        } while (x <= 0);
+
+        System.out.println("You entered " + x);
+        sc.close();
+    }    
+}
