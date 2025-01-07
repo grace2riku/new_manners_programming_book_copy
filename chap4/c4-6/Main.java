828ca1009c210799c3b0bb82c4dfc7a4664c3eaf
Koji Abe <gracehime@gmail.com>
2025-01-07 11:09:40 +0900

    Listing 4.6 ファイル入出力を行うプログラム例の追加

---
+++ chap4/c4-6/Main.java
@@ -0,0 +1,18 @@
+// Listing 4.6 ファイル入出力を行うプログラム例
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) {
+        Path path = Path.of("data.csv");
+        try (var in = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
