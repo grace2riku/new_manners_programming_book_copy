2d6b94c471a498645be680720c6063e614a41824
Koji Abe <gracehime@gmail.com>
2025-01-09 12:31:30 +0900

    Listing 4.9 TreeMapを用いたプログラム例の追加

---
+++ chap4/c4-9/Main.java
@@ -0,0 +1,17 @@
+// Listing 4.9 TreeMapを用いたプログラム例
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, String> stmap = new TreeMap<>();
+        stmap.put(1001, "Alice");
+        stmap.put(1002, "Bob");
+        stmap.put(1003, "Caroline");
+        stmap.put(1004, "David");
+        stmap.put(1005, "Eve");
+        stmap.put(1007, "Giant");
+
+        System.out.println(stmap.get(1005));
+    }
+}
