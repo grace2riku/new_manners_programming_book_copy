5c82b11870c7eab092985872d2b5c02c33fb73e9
Koji Abe <gracehime@gmail.com>
2025-01-10 23:16:19 +0900

    Listing 5.2 Elixirによる入力・演算・出力の追加

---
+++ chap5/c5-2.exs
@@ -0,0 +1,12 @@
+# Listing 5.2 Elixirによる入力・演算・出力
+defmodule Calculator do
+  def main do
+    price = 150
+    IO.write "How many do you need?: "  # 改行しない出力
+    amount = IO.gets("") |> String.trim |> String.to_integer
+    total = price * amount
+    IO.puts "Total : #{total} yen"      # 出力後に改行
+  end
+end
+
+Calculator.main
