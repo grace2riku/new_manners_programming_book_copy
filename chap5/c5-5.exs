c60753f11834127a05ed76e9b61e328c7c72d0fd
Koji Abe <gracehime@gmail.com>
2025-01-12 11:18:38 +0900

    Listing 5.5 ElixirによるMap処理の追加

---
+++ chap5/c5-5.exs
@@ -0,0 +1,10 @@
+# Listing 5.5 ElixirによるMap処理
+defmodule Map1 do
+  def main do
+    name_list = ["Alice", "Bob", "Caroline", "David", "Eve"]
+    len_list = Enum.map(name_list, &String.length/1)
+    IO.inspect(len_list)
+  end
+end
+
+Map1.main
