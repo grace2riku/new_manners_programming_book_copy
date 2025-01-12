424f8d68d012dc87f622768952116f4166697d38
Koji Abe <gracehime@gmail.com>
2025-01-12 11:33:11 +0900

    Listing 5.8 クロージャを使ったFilter処理の追加

---
+++ chap5/c5-8.exs
@@ -0,0 +1,13 @@
+# Listing 5.8 クロージャを使ったFilter処理
+defmodule Filter3 do
+  def main do
+    char = "e"
+    name_list = ["Alice", "Bob", "Caroline", "David", "Eve"]
+    filtered_list = Enum.filter(name_list,
+      fn s -> String.contains?(s, char) end)
+    IO.inspect(filtered_list)
+  end
+
+end
+
+Filter3.main
