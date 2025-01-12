f530e62507b27f4a7b0b519e32784d54f2a0b877
Koji Abe <gracehime@gmail.com>
2025-01-12 11:22:22 +0900

    Listing 5.6 ElixirによるFilter処理の追加

---
+++ chap5/c5-6.exs
@@ -0,0 +1,12 @@
+# Listing 5.6 ElixirによるFilter処理
+defmodule Filter1 do
+  def main do
+    name_list = ["Alice", "Bob", "Caroline", "David", "Eve"]
+    filtered_list = Enum.filter(name_list, &has_a?/1)
+    IO.inspect(filtered_list)
+  end
+
+  defp has_a?(s), do: String.contains?(s, "a")
+end
+
+Filter1.main
